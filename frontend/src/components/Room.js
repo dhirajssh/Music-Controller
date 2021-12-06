034e81e0b9f517fa3c8b88fb710afe3f606b28d4
Dhiraj Shah <dhirajssh@gmail.com>
2021-12-06 15:45:30 +0530

    play and pause drf views

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
-import { Grid, Button, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
-import CreateRoomPage from './CreateRoomPage';
+import React, { Component } from "react";
+import { Grid, Button, Typography } from "@material-ui/core";
+import CreateRoomPage from "./CreateRoomPage";
+import MusicPlayer from "./MusicPlayer";
 
 export default class Room extends Component {
   constructor(props) {
@@ -12,6 +12,7 @@ export default class Room extends Component {
       isHost: false,
       showSettings: false,
       spotifyAuthenticated: false,
+      song: {},
     };
     this.roomCode = this.props.match.params.roomCode;
     this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
@@ -20,11 +21,20 @@ export default class Room extends Component {
     this.renderSettings = this.renderSettings.bind(this);
     this.getRoomDetails = this.getRoomDetails.bind(this);
     this.authenticateSpotify = this.authenticateSpotify.bind(this);
+    this.getCurrentSong = this.getCurrentSong.bind(this);
     this.getRoomDetails();
   }
 
-  getRoomDetails = () => {
-    fetch('/api/get-room?code='+this.roomCode)
+  componentDidMount() {
+    this.interval = setInterval(this.getCurrentSong, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  getRoomDetails() {
+    return fetch("/api/get-room" + "?code=" + this.roomCode)
       .then((response) => {
         if (!response.ok) {
           this.props.leaveRoomCallback();
@@ -33,81 +43,102 @@ export default class Room extends Component {
         return response.json();
       })
       .then((data) => {
-        console.log(data)
         this.setState({
           votesToSkip: data.votes_to_skip,
           guestCanPause: data.guest_can_pause,
           isHost: data.is_host,
         });
-        if (this.state.isHost)
+        if (this.state.isHost) {
           this.authenticateSpotify();
-      })
-  }
-
-  authenticateSpotify = () => {
-    fetch("/spotify/is-authenticated")
-      .then((response)=>response.json())
-      .then((data) => {
-        this.setState({ spotifyAuthenticated: data.status });
-        if (!data.status) {
-          fetch('/spotify/get-auth-url')
-            .then((response) => response.json())
-            .then((data)=>{
-              window.location.replace(data.url);
-            })
         }
       });
   }
 
-  leaveButtonPressed = () => {
-    const requestOptions = {
-      method: 'POST',
-      headers: {"Content-Type": "application/json"},
-    };
-    fetch("/api/leave-room", requestOptions)
-      .then((response) => {
-        this.props.leaveRoomCallback();
-        this.props.history.push("/");
-      })
+  authenticateSpotify() {
+    fetch("/spotify/is-authenticated")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ spotifyAuthenticated: data.status });
+        console.log(data.status);
+        if (!data.status) {
+          fetch("/spotify/get-auth-url")
+            .then((response) => response.json())
+            .then((data) => {
+              window.location.replace(data.url);
+            });
+        }
+      });
   }
 
-  updateShowSettings = (value) => {
+  getCurrentSong() {
+    fetch("/spotify/current-song")
+      .then((response) => {
+        if (!response.ok) {
+          return {};
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        this.setState({ song: data });
+        console.log(this.state.song);
+      });
+  }
+
+  leaveButtonPressed() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/api/leave-room", requestOptions).then((_response) => {
+      this.props.leaveRoomCallback();
+      this.props.history.push("/");
+    });
+  }
+
+  updateShowSettings(value) {
     this.setState({
       showSettings: value,
-    })
+    });
   }
 
-  renderSettingsButton= () => {
+  renderSettings() {
     return (
-      <Grid item xs={12} align="center">
-        <Button variant="contained" color="primary" onClick={()=>{this.updateShowSettings(true)}}>SETTings</Button>
-      </Grid>
-    )
-  }
-
-  renderSettings = () => {
-    return(
       <Grid container spacing={1}>
         <Grid item xs={12} align="center">
-          <CreateRoomPage 
-            update={true} 
-            votesToskip={this.state.votesToSkip}  
-            guestCanPause={this.guestCanPause}
+          <CreateRoomPage
+            update={true}
+            votesToSkip={this.state.votesToSkip}
+            guestCanPause={this.state.guestCanPause}
             roomCode={this.roomCode}
             updateCallback={this.getRoomDetails}
           />
         </Grid>
         <Grid item xs={12} align="center">
-          <Button 
-            variant="contained" 
-            color="secondary" 
-            onClick={()=>this.updateShowSettings(false)}
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => this.updateShowSettings(false)}
           >
             Close
           </Button>
         </Grid>
       </Grid>
-    )
+    );
+  }
+
+  renderSettingsButton() {
+    return (
+      <Grid item xs={12} align="center">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => this.updateShowSettings(true)}
+        >
+          Settings
+        </Button>
+      </Grid>
+    );
   }
 
   render() {
@@ -117,40 +148,22 @@ export default class Room extends Component {
     return (
       <Grid container spacing={1}>
         <Grid item xs={12} align="center">
-          <Typography variant="h6" component="h6">
+          <Typography variant="h4" component="h4">
             Code: {this.roomCode}
           </Typography>
         </Grid>
-        <Grid item xs={12} align="center">
-          <Typography variant="h6" component="h6">
-            Votes: {this.state.votesToSkip}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Typography variant="h6" component="h6">
-            Guest can Pause: {this.state.guestCanPause.toString()}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Typography variant="h6" component="h6">
-            Host: {this.state.isHost.toString()}
-          </Typography>
-        </Grid>
+        <MusicPlayer {...this.state.song} />
         {this.state.isHost ? this.renderSettingsButton() : null}
         <Grid item xs={12} align="center">
-          <Button color="secondary" variant="contained" onClick={this.leaveButtonPressed}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={this.leaveButtonPressed}
+          >
             Leave Room
           </Button>
         </Grid>
       </Grid>
-    )
+    );
   }
 }
-
-
-{/* <div>
-        <h3>{this.roomCode}</h3>
-        <p>Votes: {this.state.votesToSkip}</p>
-        <p>Guest can Pause: {this.state.guestCanPause.toString()}</p>
-        <p>Host: {this.state.isHost.toString()}</p>
-      </div> */}
--- spotify/views.py
+++ spotify/views.py
@@ -66,6 +66,54 @@ class CurrentSong(APIView):
     host = room.host
     endpoint = "player/currently-playing"
     response = execute_spotify_api_requests(host, endpoint)
-    print(response)
+    
+    if 'error' in response or 'item' not in response:
+      return Response({}, status=status.HTTP_204_NO_CONTENT)
 
-    return Response(response, status=status.HTTP_200_OK)
+    item = response.get('item')
+    duration = item.get('duration_ms')
+    progress = response.get('progress_ms')
+    album_cover = item.get('album').get('images')[0].get('url')
+    is_playing = response.get('is_playing')
+    song_id = item.get('id')
+
+    artist_string = ""
+
+    for i, artist in enumerate(item.get('artists')):
+      if i > 0:
+        artist_string += ", "
+      name = artist.get('name')
+      artist_string += name
+
+    song = {
+      'title': item.get('name'),
+      'artist': artist_string,
+      'duration': duration,
+      'time': progress,
+      'image_url': album_cover,
+      'is_playing': is_playing,
+      'votes': 0,
+      'id': song_id
+    }
+
+    return Response(song, status=status.HTTP_200_OK)
+
+class PauseSong(APIView):
+  def put(self, response, format=None):
+    room_code = self.request.session.get('room_code')
+    room = Room.objects.filter(code=room_code)[0]
+    if self.request.session.session_key == room.host or room.guest_can_pause:
+      pause_song(room.host)
+      return Response({}, status=status.HTTP_204_NO_CONTENT)
+    
+    return Response({}, status=status.HTTP_403_FORBIDDEN)
+
+class PlaySong(APIView):
+  def put(self, response, format=None):
+    room_code = self.request.session.get('room_code')
+    room = Room.objects.filter(code=room_code)[0]
+    if self.request.session.session_key == room.host or room.guest_can_pause:
+      play_song(room.host)
+      return Response({}, status=status.HTTP_204_NO_CONTENT)
+    
+    return Response({}, status=status.HTTP_403_FORBIDDEN)
