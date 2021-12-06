9f85388c9138fef63dca25e1d6123b474e76f2d1
Dhiraj Shah <dhirajssh@gmail.com>
2021-12-06 09:35:29 +0530

    spotify api backend

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import index
 
+app_name = 'frontend'
+# the reason app_name is assigned is cause django needs to know that the urls.py file belongs to the frontend app
+
+# name if one redirects to 'frontend:hello' it would be redirected index if its name is hello
+
 urlpatterns = [
-    path('', index),
+    path('', index, name=''),
     path('join', index),
     path('create',index),
     path('room/<str:roomCode>', index)
